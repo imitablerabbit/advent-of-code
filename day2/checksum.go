e4ebe66b9ff042aac9d8991c943712d2fd4d8428
Mark Hillman <markhillman94@googlemail.com>
2017-12-16 15:45:44 +0000

    Add day2 task 1

---
+++ day2/checksum.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	spreadsheetFlag = flag.String("spreadsheet", "", "`file` of spreadsheet data to compute checksum for")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func computeRowDiff(row []int) int {
+	lowest := row[0]
+	highest := row[0]
+	for _, num := range row {
+		if num < lowest {
+			lowest = num
+		}
+		if num > highest {
+			highest = num
+		}
+	}
+	return highest - lowest
+}
+
+func computeChecksum(spreadsheet [][]int) (checksum int) {
+	for _, row := range spreadsheet {
+		diff := computeRowDiff(row)
+		checksum = checksum + diff
+	}
+	return
+}
+
+func parseSpreadsheetString(spreadsheet string) (data [][]int, err error) {
+	trimmedSpreadsheet := strings.Trim(spreadsheet, "\n")
+	rowStrings := strings.Split(trimmedSpreadsheet, "\n")
+	for _, rowString := range rowStrings {
+		whitespaceRegex := regexp.MustCompile("\\s+") // Any whitespace
+		numberStrings := whitespaceRegex.Split(rowString, -1)
+		var rowDigits []int
+		for _, numberString := range numberStrings {
+			digit, err := strconv.Atoi(numberString)
+			if err != nil {
+				return data, err
+			}
+			rowDigits = append(rowDigits, digit)
+		}
+		data = append(data, rowDigits)
+	}
+	return
+}
+
+func main() {
+	if *spreadsheetFlag == "" {
+		flag.PrintDefaults()
+		return
+	}
+	spreadsheetBytes, err := ioutil.ReadFile(*spreadsheetFlag)
+	if err != nil {
+		fmt.Printf("Error: read spreadsheet file: %v\n", err)
+		return
+	}
+	spreadsheetData, err := parseSpreadsheetString(string(spreadsheetBytes))
+	if err != nil {
+		fmt.Printf("Error: could not parse spreadsheet data: %v\n%v", err, string(spreadsheetBytes))
+		return
+	}
+	checksum := computeChecksum(spreadsheetData)
+	fmt.Printf("Spreadsheet = %v\nChecksum = %d\n", spreadsheetData, checksum)
+}
